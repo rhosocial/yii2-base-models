18a9701ef65436fe67296b2a2dd76f899e3ab17f
Zhao vistart <i@vistart.vip>
2017-03-11 21:38:29 +0800

    add special User component.

---
+++ web/User.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ *  _   __ __ _____ _____ ___  ____  _____
+ * | | / // // ___//_  _//   ||  __||_   _|
+ * | |/ // /(__  )  / / / /| || |     | |
+ * |___//_//____/  /_/ /_/ |_||_|     |_|
+ * @link https://vistart.me/
+ * @copyright Copyright (c) 2016 - 2017 vistart
+ * @license https://vistart.me/license/
+ */
+
+namespace rhosocial\base\models\web;
+
+use rhosocial\base\models\models\BaseUserModel;
+
+/**
+ * @version 1.0
+ * @author vistart <i@vistart.me>
+ */
+class User extends \yii\web\User
+{
+    private $_access = [];
+    
+    public function getGuid()
+    {
+        $identity = $this->getIdentity();
+        /* @var $identity BaseUserModel */
+        return $identity !== null ? $identity->getGUID() : null;
+    }
+    
+    public function can($permissionName, $params = [], $allowCaching = true)
+    {
+        if ($allowCaching && empty($params) && isset($this->_access[$permissionName])) {
+            return $this->_access[$permissionName];
+        }
+        if (($accessChecker = $this->getAccessChecker()) === null) {
+            return false;
+        }
+        $access = $accessChecker->checkAccess($this->getGuid(), $permissionName, $params);
+        if ($allowCaching && empty($params)) {
+            $this->_access[$permissionName] = $access;
+        }
+
+        return $access;
+    }
+}
