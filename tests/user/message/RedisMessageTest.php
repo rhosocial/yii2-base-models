d3eabdef4f7db41b391ccb25c58b1e0a0c199190
vistart <i@vistart.me>
2021-10-01 08:52:48 +0800

    Sleep 5 seconds before checking it exists.

--- tests/user/message/RedisMessageTest.php
+++ tests/user/message/RedisMessageTest.php
@@ -134,7 +134,7 @@ class RedisMessageTest extends UserTestCase
         $this->assertTrue($message->save());
         $message_id = $message->getGUID();
         
-        sleep(1);
+        sleep(5);
         $this->assertEquals(0, RedisMessage::find()->byIdentity($this->user)->read()->count());
         $this->assertEquals(1, RedisMessage::find()->byIdentity($this->user)->unread()->count());
         $this->assertEquals(0, RedisMessage::find()->byIdentity($this->other)->read()->count());
